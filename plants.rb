42ff9b43bd9efe1a0db452d30e7ed38bd07c3646
Joop Stark <stark.joop@gmail.com>
2023-05-16 20:59:55 -0600

    Change if statement

--- plants.rb
+++ plants.rb
@@ -1,8 +1,10 @@
 gets_sunshine = true
+ounces_of_water = 10
+
 ounces_of_water = 1
  
-if gets_sunshine && ounces_of_water >= 10
+if gets_sunshine || ounces_of_water >= 10
     puts "It lives"
-else
+ else
    puts "Dies"
-end 
+ end 
